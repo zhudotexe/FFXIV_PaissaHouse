218c9b4eb2e1720f131247ffb80be70be06cd763
Andrew Zhu <me@andrew-zhu.com>
2021-04-21 18:08:19 -0700

    empty out owner string if plot not owned

--- Structures/HousingWardInfo.cs
+++ Structures/HousingWardInfo.cs
@@ -12,24 +12,22 @@ namespace AutoSweep.Structures
         public static unsafe HousingWardInfo Read(IntPtr dataPtr)
         {
             HousingWardInfo wardInfo = new HousingWardInfo();
-            using (UnmanagedMemoryStream unmanagedMemoryStream = new UnmanagedMemoryStream((byte*)dataPtr.ToPointer(), 2656L))
-            {
-                using (BinaryReader binaryReader = new BinaryReader(unmanagedMemoryStream))
-                {
+            using (UnmanagedMemoryStream unmanagedMemoryStream = new UnmanagedMemoryStream((byte*)dataPtr.ToPointer(), 2656L)) {
+                using (BinaryReader binaryReader = new BinaryReader(unmanagedMemoryStream)) {
                     wardInfo.LandIdent = LandIdent.ReadFromBinaryReader(binaryReader);
                     wardInfo.HouseInfoEntries = new HouseInfoEntry[60];
 
-                    for (int i = 0; i < 60; i++)
-                    {
+                    for (int i = 0; i < 60; i++) {
                         HouseInfoEntry infoEntry = new HouseInfoEntry();
                         infoEntry.HousePrice = binaryReader.ReadUInt32();
                         infoEntry.InfoFlags = (HousingFlags)binaryReader.ReadByte();
                         infoEntry.HouseAppeals = new sbyte[3];
-                        for (int j = 0; j < 3; j++)
-                        {
+                        for (int j = 0; j < 3; j++) {
                             infoEntry.HouseAppeals[j] = binaryReader.ReadSByte();
                         }
-                        infoEntry.EstateOwnerName = Encoding.UTF8.GetString(binaryReader.ReadBytes(32)).TrimEnd(new char[1]);
+                        infoEntry.EstateOwnerName = (infoEntry.InfoFlags & HousingFlags.PlotOwned) != 0
+                            ? Encoding.UTF8.GetString(binaryReader.ReadBytes(32)).TrimEnd(new char[1])
+                            : "";
                         wardInfo.HouseInfoEntries[i] = infoEntry;
                     }
                 }
